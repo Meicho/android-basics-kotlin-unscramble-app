58390c43685de0ee769d23eb0aef1662f3d39164
Meicho <meichohendra31@gmail.com>
2023-09-22 16:10:42 +0700

    Di GameFragment, hapus kode di dalam metode onSubmitWord()dan onSkipWord()

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -77,11 +77,7 @@ class GameFragment : Fragment() {
      * Increases the word count.
      */
     private fun onSkipWord() {
-        currentScrambledWord = getNextScrambledWord()
-        currentWordCount++
-        binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
-        setErrorTextField(false)
-        updateNextWordOnScreen()
+
     }
 
     /*
